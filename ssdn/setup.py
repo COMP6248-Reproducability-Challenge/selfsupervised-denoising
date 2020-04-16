5aeacda76f04517b0d5da56f84483fbf7e78b81e
David Jones <davidsamueljones@icloud.com>
2020-04-16 13:07:13 +0100

    Added h5py requirement

--- ssdn/setup.py
+++ ssdn/setup.py
@@ -4,7 +4,7 @@ exec(open("ssdn/version.py").read())
 
 setup(
     name="ssdn",
-    version=__version__,
+    version=__version__,  # noqa
     packages=find_packages(),
-    install_requires=["nptyping"],
+    install_requires=["nptyping", "h5py"],
 )
