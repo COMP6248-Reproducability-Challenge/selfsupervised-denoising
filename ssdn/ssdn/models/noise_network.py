045077e3ff18711fba7b3dc30338cff861eff551
David Jones <davidsamueljones@icloud.com>
2020-04-29 01:56:13 +0100

    Swapped learnable ConvTranspose2d upsamples for nearest neighbour upsampling Split out aspect of encode from decode block Fixed weight initialisations to match Kaiming He et al.
    Results within rounding error of reference Tensorflow model.

--- ssdn/ssdn/models/noise_network.py
+++ ssdn/ssdn/models/noise_network.py
@@ -89,15 +89,17 @@ class NoiseNetwork(nn.Module):
         self.encode_block_4 = _encode_block_2_3_4_5()
         self.encode_block_5 = _encode_block_2_3_4_5()
 
+        # Layers: enc_conv6
+        self.encode_block_6 = nn.Sequential(
+            self.Conv2d(48, 48, 3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+        )
+
         ####################################
         # Decode Blocks
         ####################################
-        # Layers: enc_conv6, upsample5
-        self.decode_block_6 = nn.Sequential(
-            self.Conv2d(48, 48, 3, stride=1, padding=1),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
-            nn.ConvTranspose2d(48, 48, 3, stride=2, padding=1, output_padding=1),
-        )
+        # Layers: upsample5
+        self.decode_block_6 = nn.Sequential(nn.Upsample(scale_factor=2, mode="nearest"))
 
         # Layers: dec_conv5a, dec_conv5b, upsample4
         self.decode_block_5 = nn.Sequential(
@@ -105,7 +107,7 @@ class NoiseNetwork(nn.Module):
             nn.LeakyReLU(negative_slope=0.1, inplace=True),
             self.Conv2d(96, 96, 3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True),
-            nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1),
+            nn.Upsample(scale_factor=2, mode="nearest"),
         )
 
         # Layers: dec_deconv(i)a, dec_deconv(i)b, upsample(i-1); i=4..2
@@ -115,7 +117,7 @@ class NoiseNetwork(nn.Module):
                 nn.LeakyReLU(negative_slope=0.1, inplace=True),
                 self.Conv2d(96, 96, 3, stride=1, padding=1),
                 nn.LeakyReLU(negative_slope=0.1, inplace=True),
-                nn.ConvTranspose2d(96, 96, 3, stride=2, padding=1, output_padding=1),
+                nn.Upsample(scale_factor=2, mode="nearest"),
             )
 
         # Separate instances of same decode module definition created
@@ -162,14 +164,21 @@ class NoiseNetwork(nn.Module):
 
     def _init_weights(self):
         """Initializes weights using Kaiming  He et al. (2015).
+
+        Only convolution layers have learnable weights. All convolutions use a leaky
+        relu activation function (negative_slope = 0.1) except the last which is just
+        a linear output.
         """
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight.data)
+                nn.init.kaiming_normal_(m.weight.data, a=0.1)
                 m.bias.data.zero_()
+        # Initialise last output layer
         if self._zero_output_weights:
             self.output_conv.weight.zero_()
+        else:
+            nn.init.kaiming_normal_(self.output_conv.weight.data, nonlinearity="linear")
 
     def forward(self, x: Tensor) -> Tensor:
         if self.blindspot:
@@ -182,9 +191,10 @@ class NoiseNetwork(nn.Module):
         pool3 = self.encode_block_3(pool2)
         pool4 = self.encode_block_4(pool3)
         pool5 = self.encode_block_5(pool4)
+        encoded = self.encode_block_6(pool5)
 
         # Decoder
-        upsample5 = self.decode_block_6(pool5)
+        upsample5 = self.decode_block_6(encoded)
         concat5 = torch.cat((upsample5, pool4), dim=1)
         upsample4 = self.decode_block_5(concat5)
         concat4 = torch.cat((upsample4, pool3), dim=1)
@@ -212,7 +222,8 @@ class NoiseNetwork(nn.Module):
 
         return x
 
-    def input_wh_mul(self) -> int:
+    @staticmethod
+    def input_wh_mul() -> int:
         """Multiple that both the width and height dimensions of an input must be to be
         processed by the network. This is devised from the number of pooling layers that
         reduce the input size.
